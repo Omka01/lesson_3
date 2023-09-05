2028ddb84cbd2e2392fc5ecd673a2e13951fad2a
Omka01 <t_omka01@mail.ru>
2023-09-05 22:51:48 +0600

    Initial commit

---
+++ Dart_lesson/classwork.dart
@@ -0,0 +1,10 @@
+void main() {
+//  var a = 5;
+//  var b = 5.126;
+//  var c = (a / b);
+//  print(c);
+  int a = 5;
+  int b = 2;
+  int c = (a + b);
+  print("5 + 2 = $c");
+}
---
+++ Dart_lesson/test.dart
@@ -0,0 +1,10 @@
+import 'dart:io';
+
+void main() {
+  //////////////////////////////
+  print("you Name");
+
+  String? userAnwser = stdin.readLineSync();
+
+  print("You name $userAnwser");
+}
